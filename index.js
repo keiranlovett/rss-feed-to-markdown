8c7bffa0ccdedebadda783d1e416c131478d724d
matthewpquantumware <102489376+matthewpquantumware@users.noreply.github.com>
2023-12-24 19:50:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ async function run() {
     // Parse the XML feed
     const feedData = await parseStringPromise(feedXml);
     //const entries = feedData?.feed?.item || [];
-    const items = feedData?.rss?.channel?.item || [];
-    console.log(`Feed items '${items.length()}' '${items.count()}' found.`);
+    const items = feedData?.channel?.item || [];
+    console.log(`Feed items '${feedData}' found.`);
 
     // Process the feed entries and generate Markdown files
     items.forEach((item) => {
