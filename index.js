71cc61640805dfd922e4543faa6cdcd5483030f1
matthewpquantumware <102489376+matthewpquantumware@users.noreply.github.com>
2024-03-30 20:21:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,10 @@ async function run() {
 
       //console.log(item.enclosure?.[$]?.[url]);
 
-      const thumbnail =  item.enclosure?.[0]?.$?.url || '';
+      const thumbnail =  item.enclosure?.[0]?.$.url || '';
+      console.log(item.enclosure?.[0]?.$.url || '');
+      console.log(item.enclosure?.[0]?.$?.url || '');
+      
       //  item.enclosure[0] ? item.enclosure[0].$.url : '';
       
       //const thumbnail = item.enclosure?.[$]?.[url] || '';
