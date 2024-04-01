ab7a8d4ce54ab6fbcc00d1db4ba51cd936fa821b
matthewpquantumware <matthewp@quantumware.io>
2024-04-01 17:48:56 -0400

    Update getURL.js

--- getURL.js
+++ getURL.js
@@ -4,22 +4,20 @@ const axios = require('axios');
 
 async function fetchURLContent(url) {
     const article = '';
-    try {
-    const response = await axios.get(url, {
-        //We can add more configurations in this object
-           params: {
-          //This is one of the many options we can configure
-           }
-        });
-    article= response.data;
-    article = new Readability(article).parse();
-    } catch (err) {
-        console.log(err +" Error Parsing: " + url);
-    }
+    //try {
+    //    const response = await axios.get(url, {
+    //        //We can add more configurations in this object
+    //        params: {
+            //This is one of the many options we can configure
+    //        }
+    //        });
+    ///    article= response.data;
+    //    article = new readability(article).parse();
+    //} catch (err) {
+    //    console.log(err +" Error Parsing: " + url);
+    //}
     return article;
 }
-
-
 
 module.exports = {
     fetchURLContent
