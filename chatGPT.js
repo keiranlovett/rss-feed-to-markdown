315f50a9fc9dc6dc2f1c598c9b7f0e780833cb64
matthewpquantumware <matthewp@quantumware.io>
2024-04-01 17:17:03 -0400

    chatgpt

---
+++ chatGPT.js
@@ -0,0 +1,39 @@
+const core = require('@actions/core');
+const axios = require('axios');
+
+const apiUrl = trim(core.getInput('OPENAI_apiUrl')|| 'https://api.openai.com/v1/chat/completions');
+const apiKey = trim(core.getInput('OPENAI_apiKey')|| '');
+const model = trim(core.getInput('OPENAI_model')|| 'gpt-3.5-turbo');
+
+//const apiUrl = trim(process.env.apiUrl || 'https://api.openai.com/v1/chat/completions');
+//const apiKey = trim(process.env.apiKey || ''); // @Please add here your openai key.
+
+async function fetchChatCompletion(chatMessageContent) {
+    try {
+        const response = await axios.post(
+            apiUrl,
+            {
+                messages: [
+                    { role: 'system', content: chatMessageContent },
+                ],
+                model: model, // Specify the model to use
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${apiKey}`,
+                },
+            }
+        );
+
+        console.log('Chat completion response:', response.data.choices);
+        return response.data.choices;
+    } catch (error) {
+        console.error('Error:', error.response.data);
+    }
+}
+
+
+module.exports = {
+    fetchChatCompletion
+}
